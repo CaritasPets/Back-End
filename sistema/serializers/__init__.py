c7e9f6b47c8fdca43a09e26ccd6166d4f8471d83
Johann5731 <johannmborges27@gmail.com>
2025-07-04 14:49:45 -0300

    FEAT: Serializers criados #3

---
+++ sistema/serializers/__init__.py
@@ -0,0 +1,6 @@
+from .organizacao import OrganizacaoSerializer
+from .pet import PetSerializer
+from .porte import PorteSerializer
+from .raca import RacaSerializer
+from .usuario import UsuarioSerializer
+from .voluntario import VoluntarioSerializer
---
+++ sistema/serializers/raca.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from models import Raca
+
+
+class RacaSerializer(ModelSerializer):
+   class Meta:
+       model = Raca
+       fields = "__all__"
