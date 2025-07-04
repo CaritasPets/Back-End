69e3c5d2504a7a6fffbff72b8e56b954714506a9
AndreRicardoSc <andrenarutto7@gmail.com>
2025-07-04 13:52:01 -0300

    FEAT: Criado a model para usuario #1

--- sistema/models/__init__.py
+++ sistema/models/__init__.py
@@ -2,3 +2,4 @@ from .organizacao import Organizacao
 from .porte import Porte
 from .raca import Raca
 from .voluntario import Voluntario
+from .usuario import Usuario
---
+++ sistema/models/usuario.py
@@ -0,0 +1,16 @@
+from django.db import models 
+
+class Usuario(models.Model):
+    cpf = models.CharField(max_length=11, unique=True, null=False, blank=False)
+    username = models.CharField(max_length=45, unique=True, null=False, blank=False)
+    senha = models.CharField(max_length=30, null=False, blank=False)
+    telefone = models.CharField(max_length=11, unique=True, null=False, blank=False)
+    email = models.CharField(max_length=320, unique=True, null=False, blank=False)
+    nome_completo = models.CharField(max_length=100, null=True, blank=True)
+    endereco = models.CharField(max_length=100, null=True, blank=True)
+    data_nascimento = models.DateField
+
+    def __str__(self):
+        return f'{self.username} ({self.cpf})'
+
+
