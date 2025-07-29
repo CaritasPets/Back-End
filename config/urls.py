06af6d4a5210d13b83396c51fb15c2217db7eb74
AndreRicardoSc <andrenarutto7@gmail.com>
2025-07-29 13:55:40 -0300

    FIX: Urls para acesso aos tokens e refreshes alterados #11

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ router.register(r"voluntarios", VoluntarioViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('token/refresh', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
 ]
