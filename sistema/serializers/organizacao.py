e67ee6d340afaf67bf5ccfad2366e8e9e0adab82
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-10 22:16:30 -0300

    FEAT: Implementando a organization com admin #20

--- sistema/serializers/organizacao.py
+++ sistema/serializers/organizacao.py
@@ -3,6 +3,11 @@ from ..models import Organizacao
 
 
 class OrganizacaoSerializer(ModelSerializer):
-   class Meta:
+    class Meta:
        model = Organizacao
        fields = "__all__"
+       depth = 1
+    def create(self, validated_data):
+        user = self.context["request"].user
+        return Organizacao.objects.create(admin=user, **validated_data)
+
--- sistema/views/organizacao.py
+++ sistema/views/organizacao.py
@@ -1,8 +1,17 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ..models import Organizacao
 from ..serializers import OrganizacaoSerializer
 
 
+
 class OrganizacaoViewSet(ModelViewSet):
    queryset = Organizacao.objects.all()
    serializer_class = OrganizacaoSerializer
+   permission_classes = [IsAuthenticated]
+
+   def get_serializer_context(self):
+      context = super().get_serializer_context()
+      context["request"] = self.request
+      return context
+
