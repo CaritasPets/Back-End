bf8e43863b611916ea96aa2bfb1cc6ee4d99b248
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-17 07:51:20 -0300

    FEAT: Fazendo ajustes para ficar finalmente pronto

--- sistema/models/raca.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Raca(models.Model):
-    tipo = models.CharField(max_length=45)
-    descricao = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.tipo}'
--- sistema/serializers/raca.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from ..models import Raca
-
-
-class RacaSerializer(ModelSerializer):
-   class Meta:
-       model = Raca
-       fields = "__all__"
