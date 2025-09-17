cf1f0f8ee919f3a9508764e5155cc39cbfcc2eb1
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-17 16:25:07 -0300

    FEAT: Criando ambiente para deploy

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,10 +129,11 @@ FILE_UPLOAD_PERMISSIONS = 0o640
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),  # Render fornece essa variável
+        conn_max_age=600,  # mantém conexões abertas para performance
+        ssl_require=True   # força SSL no banco do Render
+    )
 }
 
 
